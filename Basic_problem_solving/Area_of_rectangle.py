f56a3a5192958129a825d8a06df537492e988a56
venukumarsakalabhaktula-blip <venukumarsakalabhaktula@gmail.com>
2026-02-15 11:10:13 +0530

    Create Area_of_rectangle.py

---
+++ Basic_problem_solving/Area_of_rectangle.py
@@ -0,0 +1,9 @@
+def area_of_rectangle(a,b):
+    area=a*b
+    return area
+    
+a=int(input("enter the length of rectangle: ")) 
+b=int(input("enter the breadth of rectangle: "))
+
+c=area_of_rectangle(a,b)
+print(c)
