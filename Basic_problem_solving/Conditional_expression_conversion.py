b019bfad9f6f3a97178428bcc0f7224fc962f7aa
venukumarsakalabhaktula-blip <venukumarsakalabhaktula@gmail.com>
2026-02-16 22:55:19 +0530

    Update Conditional_expression_conversion.py

--- Basic_problem_solving/Conditional_expression_conversion.py
+++ Basic_problem_solving/Conditional_expression_conversion.py
@@ -1,5 +1,19 @@
-a=int(input("enter value of a:"))
-b=int(input("enter value of b:"))
-x= a + b
-if(a>10 and b<5) else 0
-print("value of x:",x)
+
+#condital
+a = int(input("Enter value of a: "))
+b = int(input("Enter value of b: "))
+
+x = a + b if (a > 10 and b < 5) else 0
+
+print("Value of x:", x)
+
+
+
+
+#pass or fail
+marks = int(input("Enter marks: "))
+
+if marks > 60:
+    print("Passed")
+else:
+    print("Fail")
